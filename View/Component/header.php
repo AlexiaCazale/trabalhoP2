59f015258213244d4ba8e1f0730088de5f44494f
AlexiaCazale <alexiacazale7@gmail.com>
2025-06-01 22:57:24 -0300

    feat: adicionando logo

--- View/Component/header.php
+++ View/Component/header.php
@@ -22,9 +22,9 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarPillsExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <ul class="navbar-nav navbar-nav-pills" style="display: flex; align-items: center; gap: 20px;">
-      <a class="navbar-brand" href="#"><img src="../Images/logo.png" width="36" /></a>
-        <h4>
+    <ul class="navbar-nav navbar-nav-pills" style="display: flex; align-items: center;">
+      <a class="navbar-brand" href="#"><img src="View/Images/logo.png" width="40" /></a>
+        <h4 style="margin: 0;"> 
         TaskHub
 		    </h4>
     </ul>
