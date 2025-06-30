ce19dcdbb48a2a289338521e0e26c375f37e431d
Lallindo <brunolallo8@gmail.com@>
2025-06-30 12:21:31 -0300

    feat: Banco atualizado

--- task_hub.sql
+++ task_hub.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 30/06/2025 às 02:19
+-- Host: localhost
+-- Tempo de geração: 30/06/2025 às 17:20
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -32,24 +32,11 @@ CREATE TABLE `atividade` (
   `id_workspace_fk` int(11) NOT NULL,
   `nome_atividade` varchar(500) NOT NULL,
   `descricao_atividade` varchar(5000) NOT NULL,
-  `data_entrega_atividade` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `data_entrega_atividade` datetime NOT NULL,
   `data_criacao_atividade` timestamp NOT NULL DEFAULT current_timestamp(),
   `ativo_atividade` tinyint(1) NOT NULL DEFAULT 1,
   `concluido_atividade` tinyint(1) NOT NULL DEFAULT 0,
   `data_concluido_atividade` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `comentario`
---
-
-CREATE TABLE `comentario` (
-  `id_comentario` int(11) NOT NULL,
-  `id_atividade_fk` int(11) NOT NULL,
-  `id_usuario_fk` int(11) NOT NULL,
-  `texto_comentario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -101,6 +88,7 @@ CREATE TABLE `workspace` (
   `id_workspace` int(11) NOT NULL,
   `nome_workspace` varchar(200) NOT NULL,
   `descricao_workspace` varchar(1000) NOT NULL,
+  `id_usuario_admin_fk` int(11) NOT NULL,
   `ativo_workspace` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -117,18 +105,11 @@ ALTER TABLE `atividade`
   ADD KEY `concluido_atividade` (`concluido_atividade`);
 
 --
--- Índices de tabela `comentario`
---
-ALTER TABLE `comentario`
-  ADD PRIMARY KEY (`id_comentario`),
-  ADD KEY `id_atividade_fk` (`id_atividade_fk`,`id_usuario_fk`),
-  ADD KEY `id_usuario_fk` (`id_usuario_fk`);
-
---
 -- Índices de tabela `membro_atividade`
 --
 ALTER TABLE `membro_atividade`
   ADD PRIMARY KEY (`id_membro_atividade`),
+  ADD UNIQUE KEY `unique_atividade_usuario` (`id_atividade_fk`,`id_usuario_fk`),
   ADD KEY `id_usuario_fk` (`id_usuario_fk`,`id_atividade_fk`),
   ADD KEY `id_atividade_fk` (`id_atividade_fk`);
 
@@ -137,6 +118,7 @@ ALTER TABLE `membro_atividade`
 --
 ALTER TABLE `membro_workspace`
   ADD PRIMARY KEY (`id_membro_workspace`),
+  ADD UNIQUE KEY `unique_workspace_usuario` (`id_workspace_fk`,`id_usuario_fk`),
   ADD KEY `id_workplace_fk` (`id_workspace_fk`,`id_usuario_fk`),
   ADD KEY `id_usuario_fk` (`id_usuario_fk`);
 
@@ -151,7 +133,8 @@ ALTER TABLE `usuario`
 -- Índices de tabela `workspace`
 --
 ALTER TABLE `workspace`
-  ADD PRIMARY KEY (`id_workspace`);
+  ADD PRIMARY KEY (`id_workspace`),
+  ADD KEY `fk_workspace_admin` (`id_usuario_admin_fk`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -198,13 +181,6 @@ ALTER TABLE `atividade`
   ADD CONSTRAINT `atividade_ibfk_1` FOREIGN KEY (`id_workspace_fk`) REFERENCES `workspace` (`id_workspace`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Restrições para tabelas `comentario`
---
-ALTER TABLE `comentario`
-  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`id_usuario_fk`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`id_atividade_fk`) REFERENCES `atividade` (`id_atividade`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Restrições para tabelas `membro_atividade`
 --
 ALTER TABLE `membro_atividade`
@@ -217,6 +193,12 @@ ALTER TABLE `membro_atividade`
 ALTER TABLE `membro_workspace`
   ADD CONSTRAINT `membro_workspace_ibfk_1` FOREIGN KEY (`id_workspace_fk`) REFERENCES `workspace` (`id_workspace`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `membro_workspace_ibfk_2` FOREIGN KEY (`id_usuario_fk`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `workspace`
+--
+ALTER TABLE `workspace`
+  ADD CONSTRAINT `fk_workspace_admin` FOREIGN KEY (`id_usuario_admin_fk`) REFERENCES `usuario` (`id_usuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
